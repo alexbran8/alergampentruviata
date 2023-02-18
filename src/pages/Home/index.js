d5aa7d87632a98969f2c12fdadf239c73790df12
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2023-02-18 17:49:37 +0200

    package.json config update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -33,8 +33,12 @@ export const Home = () => {
       <div className="cloud c3"></div>
 
       <div className="ground">
-        <div className="rock rk1"></div>
+        <div className="grass"></div> 
+        <div className="rock rk1"></div>  
         <div className="rock rk2"></div>
+        <div className="rock rk3"></div>
+        <div className="rock rk4"></div> 
+        <div className="rock rk5"></div>
       </div>
     </div>
   );
