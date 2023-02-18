aaf5a2d9b8f021f5ff46b1554b07831cee288e11
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2023-02-18 08:19:13 +0200

    initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./index.styling.css";
+
+export const Home = () => {
+  return (
+    <div className="container">
+      <div className="man">
+        <div className="body">
+          <div className="head">
+            <div className="hat"></div>
+            <div className="glasses"></div>
+            <div className="ear"></div>
+            <div className="mouth"></div>
+          </div>
+
+          <div className="arm left">
+            <div className="lower-arm"></div>
+          </div>
+          <div className="leg left">
+            <div className="lower-leg"></div>
+          </div>
+          <div className="leg right">
+            <div className="lower-leg"></div>
+          </div>
+          <div className="arm right">
+            <div className="lower-arm"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="cloud c1"></div>
+      <div className="cloud c2"></div>
+      <div className="cloud c3"></div>
+
+      <div className="ground">
+        <div className="rock rk1"></div>
+        <div className="rock rk2"></div>
+      </div>
+    </div>
+  );
+};
