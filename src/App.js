e49d551bb3c54d074b5b1c4fe0d7b112b97f0098
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2023-02-19 07:43:55 +0200

    router and layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
-import {Home} from "./pages/Home"
-import {Footer} from "./components/Footer"
+import { Home } from "./pages/Home";
+import { Layout } from "./layout";
+import {  BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <Home />
-     <Footer />
+      <Router>
+        <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        </Layout>
+      </Router>
     </div>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -27,19 +27,6 @@ export const Home = () => {
           </div>
         </div>
       </div>
-
-      <div className="cloud c1"></div>
-      <div className="cloud c2"></div>
-      <div className="cloud c3"></div>
-
-      <div className="ground">
-        <div className="grass"></div> 
-        <div className="rock rk1"></div>  
-        <div className="rock rk2"></div>
-        <div className="rock rk3"></div>
-        <div className="rock rk4"></div> 
-        <div className="rock rk5"></div>
-      </div>
     </div>
   );
 };
