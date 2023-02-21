4c428a5e12fa538685cfc2962dfe332c6f4cf978
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2023-02-21 19:30:02 +0200

    draft animation speed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
+import { Contact } from "./pages/Contact"
 import { Home } from "./pages/Home";
+import { Team } from "./pages/Team";
+import { Projects } from "./pages/Projects";
+
 import { Layout } from "./layout";
 import {  BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
@@ -10,6 +14,9 @@ function App() {
         <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
         </Layout>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./index.styling.css";
+import { setGroundSpeed } from "../../utils";
 
 export const Home = () => {
+
+useEffect(()=>{
+    setGroundSpeed(570)
+},[])
+
   return (
     <div className="container">
       <div className="man">
