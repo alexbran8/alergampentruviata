43aa101cf3ef7a93938612faa824dae1e401e4c4
Bran, Alexandru <alexandru.bran@haufe-lexware.com>
2023-04-04 19:13:40 +0300

    design update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./index.styling.css";
 import { Navbar } from "../../components/Navbar";
+import Logo from "../../assets/Logo512.svg"
 
 export const Header = () => {
   return (
     <div className="header-container">
       <div>
-        <img className="logo" src="/logo512.svg" alt="logo" />
+        <img className="logo" src={Logo} alt="logo" />
       </div>
       <Navbar />
     </div>
