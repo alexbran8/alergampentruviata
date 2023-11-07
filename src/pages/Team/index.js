16db3003dac1488f0b2a5d4f983961f1b0f1d809
Alexandru Bran <BranA>
2023-11-07 19:11:44 +0200

    feature: team members

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -1,55 +1,81 @@
 import React from "react";
 import "./index.styling.css";
-import data from "../../data/team.json"
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
-
+import data from "../../data/team.json";
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import CardHeader from "@mui/material/CardHeader";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Chip from "@mui/material/Chip"
+import LinearProgressWithLabel from "@mui/material/LinearProgress";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const TeamMember = (props) => {
-  const {firstName, lastName, age, about, hobbies} = props.data
+  const {
+    firstName,
+    lastName,
+    age,
+    about,
+    hobbies,
+    photo,
+    memberSince,
+    progress,
+    total,
+    tags
+  } = props.data;
+
   return (
-    <Card className='card'>
+    <Card className="card">
       <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
+        sx={{ height: 200 }}
+        image={photo!== "" ? require(`../../assets/team/${photo}`):null}
         title="green iguana"
       />
+      <CardHeader
+        title={`${firstName} ${lastName}`}
+        subheader={`joined: ${memberSince}`}
+      />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+        {/* <Typography gutterBottom variant="h5" component="div">
           {firstName} {lastName}
-        </Typography>
+        </Typography> */}
+    
         <Typography variant="body2" color="text.secondary">
-         {about} {age} {hobbies}
+          {about} {age} {hobbies}
         </Typography>
+        {tags?.map(tag=> <Chip label={tag} />)}
+        
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ width: '100%', mr: 1 }}>
+        <LinearProgressWithLabel variant="determinate" value={progress} />
+      </Box>
+        <Box sx={{ minWidth: 35 }}>
+          <Typography variant="body2" color="text.secondary">{`${Math.round(
+            total,
+          )}km`}</Typography>
+        </Box>
+    </Box>
       </CardContent>
       <CardActions>
         <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        {/* <Button size="small">Learn More</Button> */}
       </CardActions>
     </Card>
   );
-}
-
+};
 
 export const Team = () => {
-
   return (
-     
-      <div className="team-container">
-      {data.map(item=> {
-        return <div>
-           <TeamMember 
-           data={item}/>
-           </div>
-      })
-
-      }      
-     </div>
-  
+    <div className="team-container">
+      {data.map((item) => {
+        return (
+          <div>
+            <TeamMember data={item} />
+          </div>
+        );
+      })}
+    </div>
   );
 };
