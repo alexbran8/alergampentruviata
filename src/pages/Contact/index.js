7f5539e16c7309b9da22ba9927f39e618b3c4f5d
Alexandru Bran <BranA>
2023-11-18 20:45:40 +0200

    feature: added contact form

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,89 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./index.styling.css";
+import { TextField, Button, Typography, Box } from "@mui/material";
+import emailjs from '@emailjs/browser';
 
-export const Contact = () => {
+export  const Contact = () => {
+  const form = useRef();
+  const handleSubmit = (e) => {
+   
+    e.preventDefault(); // prevents the page from reloading when you hit “Send”
+  
+    emailjs.sendForm(process.env.EMAIL_SERVICE_ID, process.env.EMAIL_TEMPLATE_ID, form.current, process.env.EMAIL_API_KEY)
+      .then((result) => {
+          // show the user a success message
+      }, (error) => {
+          // show the user an error
+          console.log(error)
+      });
+  };
+
   return (
-    <div className="container">
- 
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: "5%",
+        marginTop: "2%"
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          maxWidth: 600,
+          mx: "auto",
+          p: 2,
+          boxShadow: 2,
+        }}
+      >
+        <Typography variant="h4" align="center" mb={2}>
+          Contacteaza-ne!
+        </Typography>
+        
+        <form  ref={form} onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            name="name"
+            label="Name"
+            margin="normal"
+            required
+          />
+          <TextField
+            fullWidth
+            label="Email"
+            name="email"
+            margin="normal"
+            required
+            type="email"
+          />
+          <TextField
+            fullWidth
+            label="Message"
+            name="message"
+            margin="normal"
+            required
+            multiline
+            rows={4}
+          />
+          <Button
+            fullWidth
+            type="submit"
+            sx={{
+              mt: 2,
+              backgroundColor: "#000",
+              color: "#fff",
+              "&:hover": {
+                backgroundColor: "#111",
+              },
+            }}
+          >
+            Submit
+          </Button>
+        </form>
+      </Box>
+    </Box>
   );
-};
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,9 +14,9 @@ export const Home = () => {
           care credem ajutând astfel comunitatea
         </p>
         <div className="join-us-container">
-          <div className="pill"><Link to="/join">IMPLICĂ-TE!</Link></div>
+          <div className="pill"><Link to="/contact">IMPLICĂ-TE!</Link></div>
           <div>Fii alături de noi!</div>
-          <div className="pill"><Link to="/support">SPRIJINĂ-NE!</Link></div>
+          <div className="pill"><Link to="/contact">SPRIJINĂ-NE!</Link></div>
         </div>
         {/* <div className="photo-container">
             <img height="150" alt="" className="photo" src="https://scontent.ftsr1-2.fna.fbcdn.net/v/t39.30808-6/350327997_934483604493500_7312516077531970307_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=TMbsre1YAdMAX9BhSkt&_nc_ht=scontent.ftsr1-2.fna&oh=00_AfD4aiml4boPB87_HSN5ITJqOqdEiLz9P8MJZCqJHuBXbg&oe=64E09659"></img>
