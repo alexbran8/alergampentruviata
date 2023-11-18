24d99a976a89fd90449770a42a924eb5e0ae633d
Alexandru Bran <BranA>
2023-11-18 07:34:01 +0200

    chore: added padding to subtitle

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,10 +6,8 @@ import Logo  from "../../assets/logo_alergam_final.png"
 export const Header = () => {
   return (
     <div className="header-container">
-      <div className="header-container">
-        <img className="logo" src={Logo} alt="logo" />
+        <img className="logo" src={Logo} alt="logo" loading="lazy"/>
         <h1 className="header-text">Alergam pentru viata</h1>
-      </div>
       <Navbar />
     </div>
   );
